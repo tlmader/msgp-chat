e057031fd2ed23b19f4ce65ed3549736c7b6d5e0
Ted Mader <tlmader.dev@gmail.com>
2017-04-19 03:38:59 -0500

    Add input loop for agent

--- src/csci4311/chat/CLIUserAgent.java
+++ src/csci4311/chat/CLIUserAgent.java
@@ -1,8 +1,6 @@
 package csci4311.chat;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Scanner;
 
 import static java.lang.System.out;
 
@@ -20,19 +18,46 @@ public class CLIUserAgent implements UserAgent {
 
   public static void main(String[] args) {
     TextMsgpClient client = new TextMsgpClient();
-    out.println(client.join("tlmader", "general"));
-    out.println(client.join("astrika", "general"));
-    out.println(client.join("hill", "programming"));
-    out.println(client.join("tlmader", "programming"));
-    out.println(client.groups());
-    out.println(client.users("general"));
-    out.println(client.users("programming"));
-    List<String> to = new ArrayList<>();
-    to.add("#general");
-    to.add("#programming");
-    out.println(client.send(new MsgpMessage("ted", to, "I'm pooping.")));
-    out.println(client.send(new MsgpMessage("astrika", to, "You're a poopster!")));
-    out.println(client.history("general"));
-    out.println(client.history("programming"));
+    Scanner sc = new Scanner(System.in);
+    String user = "tlmader";
+    while (true) {
+      out.print("@" + user + " >> ");
+      String[] input = sc.nextLine().split(" ");
+      switch (input[0]) {
+        case "join":
+          out.println(input.length == 2 ? client.join(user, input[1]) : getUsage(input[0]));
+          break;
+        case "leave":
+          out.println(input.length == 2 ? client.leave(user, input[1]) : getUsage(input[0]));
+          break;
+        case "groups":
+          out.println(client.groups());
+          break;
+        case "users":
+          out.println(input.length == 2 ? client.users(input[1]) : getUsage(input[0]));
+          break;
+        case "history":
+          out.println(input.length == 2 ? client.history(input[1]) : getUsage(input[0]));
+          break;
+      }
+    }
+  }
+
+  private static String getUsage(String command) {
+    return "usage: " + command + " <group>";
   }
 }
+//    out.println(client.join("tlmader", "general"));
+//    out.println(client.join("astrika", "general"));
+//    out.println(client.join("hill", "programming"));
+//    out.println(client.join("tlmader", "programming"));
+//    out.println(client.groups());
+//    out.println(client.users("general"));
+//    out.println(client.users("programming"));
+//    List<String> to = new ArrayList<>();
+//    to.add("#general");
+//    to.add("#programming");
+//    out.println(client.send(new MsgpMessage("ted", to, "I'm pooping.")));
+//    out.println(client.send(new MsgpMessage("astrika", to, "You're a poopster!")));
+//    out.println(client.history("general"));
+//    out.println(client.history("programming"));
